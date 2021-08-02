f734bd240f006d8366fee01d54c63f7db7b97907
Evandro C <evandrocunhajunior@gmail.com>
2021-08-02 00:46:40 -0300

    att2.0

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const images = [
+    { 'id': '1', 'url': './img/album1.jpg' },
+    { 'id': '2', 'url': './img/album2.jpg' },
+    { 'id': '3', 'url': './img/album3.jpg' },
+    { 'id': '4', 'url': './img/album4.jpg' },
+    { 'id': '5', 'url': './img/album5.jpg' }
+]
